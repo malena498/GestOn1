3b7d78a141b5bc10caa1ce206ce5806b283d4b59
AlanBertinat22 <85422883+AlanBertinat22@users.noreply.github.com>
2021-06-07 22:54:16 -0500

    dddd

--- BibliotecaClases2/Clases/Usuario.cs
+++ BibliotecaClases2/Clases/Usuario.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BibliotecaClases2.Clases
 {
-    public class Usuario
+    public class Usuario 
     {
+        [Key]
+        private int isUsuario;
     }
 }
